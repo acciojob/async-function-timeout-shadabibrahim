b39775f3e0ae22214c65ba102fd5d52374e7662d
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-16 00:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const output = document.getElementById("output");
+const  btn = document.getElementById("btn");
+
+
+async function demo(text,delay) {
+	await setTimeout(()=>{
+		output.innerHTML = text;
+	},delay)
+}
+
+btn.addEventListener('click',()=>{
+	let t= text.value;
+	let d = delay.value
+	demo(t,d)
+})
